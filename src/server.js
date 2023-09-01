df178c7f8fb138959fc1f72bc32b68fbb2fba3ec
Kaue <primosschardosim@gmail.com>
2023-09-01 12:15:24 -0300

    salvando projeto

---
+++ src/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/api/teste', (request, response) => {
+    response.send('Bora devs');
+})
+app.listen(port, () => console.log(`Running at port ${port}`));
